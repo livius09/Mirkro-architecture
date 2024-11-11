26b56f981872397897553c236f34f8b650f3b36a
livius09 <levi.wimroither@hotmail.com>
2024-11-11 22:34:15 +0100

    added more comands

--- Leviatanische/asm_compiler.py
+++ Leviatanische/asm_compiler.py
@@ -1,19 +1,40 @@
 iner="mov R1 R2 nop"
 ops={
+    "nop":0,
     "mov": 1,
     "math":2,
-    "nop":0,
-    "R1":1,
-    "R2":2,
-    "R3":3,   
+    "1ifj":3,
+    "2ifj":4,
+    "add":0,
+    "sub":1,
+    "mul":2,
+    "div":3,
+    "rem":4,
+    
+    "R1": 1,
+    "R2": 2,
+    "R3": 3,
+    "R4": 4,
+    "R5": 5,
+    "R6": 6,
+    "R7": 7,
+    "R8": 8,
+    "ji": 9, 
+    "Mi1":10,
+    "Mi2":11,
+    "Mo1":12,
+    "Mi3":13,
+    "Mi4":14,
+    "Mo2":15,
 }
 
 iner=iner.split(" ")
 
 for i in range(len(iner)):
-    iner[i]=ops[iner[i]]
+    iner[i]=hex(ops[iner[i]])
 
 print(iner)
+
 
 #mov 5 R1 
 #thats the goal
