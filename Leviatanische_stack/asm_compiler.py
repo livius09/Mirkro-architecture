a8ed8a28ab9ecf8ac957d5ef7d812109bea4958c
livius09 <levi.wimroither@hotmail.com>
2025-02-16 01:49:05 +0100

    added the stuff i forgor

--- Leviatanische_stack/asm_compiler.py
+++ Leviatanische_stack/asm_compiler.py
@@ -1,4 +1,4 @@
-iner="mov R1 R2 nop"
+
 ops={
     "nop":0,
     "mov": 1,
@@ -34,14 +34,80 @@ ops={
     "rem":4,
        
 }
+with open("Leviatanische_stack/input.txt","r") as file:
+    dat=file.readlines()
 
-iner=iner.split(" ")
-
-for i in range(len(iner)):
-    iner[i]=hex(ops[iner[i]])
-
-print(iner)
+#remove coments
+for i in range(len(dat)):
+    dat[i]=dat[i].split("#")[0].strip()
 
 
-#mov 5 R1 
-#thats the goal
+zeil=[]
+for i in range(len(dat)):
+
+    dat[i]=dat[i].strip()
+    if len(dat[i])!=0:
+        zeil.append(dat[i].split(" ")) #split the lines into the ops
+
+print("dat:")
+print(dat)
+
+labels={}
+labeln=0
+
+for k in range(len(zeil)):
+    if len(zeil[k])==1 and zeil[k][0][-1]==':':
+        labels[str(zeil[k][0])] = (k*4-labeln*4)
+        labeln+=1
+
+
+
+for el in labels.values():
+    zeil.pop(int(el/4))
+
+labels["li"]=7
+print(labels)
+
+
+#fill comands up with nop so you dont have to write it manualy # mov R2 R3 to mov R2 R3 nop
+for k in range(len(zeil)):
+    for i in range(4-len(zeil[k])):
+        zeil[k].append("nop")
+
+for k in range(len(zeil)):
+    for i in range(len(zeil[k])):
+        if len(zeil[k])==4 and zeil[k][i][-1]==':':
+            zeil[k][i]=labels[str(zeil[k][i])]
+        
+
+
+#to give better read abylti # mov 5 R2   to mov bus R2 5 
+for k in range(len(zeil)):
+    if zeil[k][0]=="mov":
+        try:
+            nu=int(zeil[k][1])
+        except:
+            continue #if a exetion acours go to the next line
+
+        zeil[k][1]="bus"
+        zeil[k][3]=nu
+
+print("used for replacing:")
+print(zeil)
+
+out="eror"
+for k in range(len(zeil)):
+    for i in range(len(zeil[k])):
+        try:
+            out=hex(ops[zeil[k][i]]) #try to get the op code from the dict
+        except KeyError as ke:
+            try:
+                n=int(zeil[k][i]) #if its no opcode try casting it to a number
+                out=hex(n)
+            except:
+                print("could not compile key not found")
+                print(f"on line {k+1},{i+1}")
+                #exit()
+
+        print(out,end=" ")
+    print()
